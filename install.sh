a227d6246ec75427d994dc6317a06ff5644baf8d
Luis Armendariz <luis.armendariz@gmail.com>
2012-07-27 08:52:57 -0700

    Added install.sh script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -x
+
+function link()
+{
+    ln -sf "${PWD}/$1" "$HOME/.$1"
+}
+
+# directories
+[ ! -d ~/.vim ] && link vim
+[ ! -d ~/.emacs.d ] && link emacs.d
+
+# files
+link bash_profile
+link bash_aliases
+link bashrc
+link gitconfig
+link gitignore
+link inputrc
+link irbrc
+link screenrc
+link tmux.conf
+link urlview
+link vimrc
+
