d78c197b13b91cceca411bf5f1bbff552ff9cdf3
Luis Armendariz <luis.armendariz@gmail.com>
2014-10-08 00:23:24 -0700

    Updated config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,21 @@
 # ~/.config/fish/config.fish
 # -----------------------------------------------------------------------------
 # Aliases {{{
+
+alias ls='ls -GF'
 alias rm='rm -i'
 alias mv='mv -i'
 alias cp='cp -ip'
 alias df='df -h'
 alias less='less -r'
 alias x='exit'
+
 # }}}
 # Settings {{{
+
+# cwd color (default green)
 set fish_color_cwd blue
+
 # }}}
 # Configure oh-my-fish {{{
 
@@ -23,13 +29,13 @@ set fish_theme ludwig1024
 # Load plugins.
 #   List of plugins can be found in ~/.oh-my-fish/plugins/*
 #   Custom plugins can be added to ~/.oh-my-fish/custom/plugins
-set fish_plugins autojump bak vi-mode vundle z
+set fish_plugins autojump bak vi-mode vundle
 
 # Path to your custom folder (default path is $FISH/custom)
 set fish_custom $HOME/dotfiles/fish/custom
 
 # Load oh-my-fish configuration
-. $fish_path/oh-my-fish.fish
+source $fish_path/oh-my-fish.fish
 
 # }}}
 # -----------------------------------------------------------------------------
---
+++ fish/custom/plugins/autojump/j.fish
@@ -0,0 +1,1 @@
+source (brew --prefix autojump)/etc/autojump.fish
