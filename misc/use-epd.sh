6c9824d66cff18bcfaa6328e016f03a2747d1414
Luis Armendariz <luis.armendariz@gmail.com>
2015-06-16 01:16:11 -0700

    Some misc scripts and placeholders

---
+++ misc/use-epd.sh
@@ -0,0 +1,9 @@
+# Source this file in order to use Enthought's EPD
+
+# Setting PATH for EPD-7.2-2
+# The orginal version is saved in .bash_profile.pysave
+PATH="/Library/Frameworks/Python.framework/Versions/Current/bin:${PATH}"
+export PATH
+
+MKL_NUM_THREADS=1
+export MKL_NUM_THREADS
