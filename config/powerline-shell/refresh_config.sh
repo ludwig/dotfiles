053fd71a1c2b524531b74b50b5f37f51545c90d4
Luis Armendariz <luis.armendariz@gmail.com>
2024-04-12 16:41:58 -0700

    Refactor refresh_config.sh
    Fixed a few bugs, refactored the paths so that they are
    available to the `convert_and_save` function,
    and added colorized output.

--- config/powerline-shell/refresh_config.sh
+++ config/powerline-shell/refresh_config.sh
@@ -6,34 +6,40 @@
 #fswatch -o powerline.yaml | xargs -n1 -I{} ./create_powerline_config.py
 
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-config_dir="${HOME}/.config/powerline-shell"
 
+# The location of the powerline-shell config directory.
+export config_dir="${HOME}/.config/powerline-shell"
+
+# The full path to the `powerline.yaml` file.
+export powerline_yaml=$(readlink -f "${script_dir}/powerline.yaml")
+
+# The path to the target `config.json`` file.
+export config_json="${config_dir}/config.json"
+
+# For colorizing the output
+export red=$(tput setaf 1)
+export green=$(tput setaf 2)
+export magenta=$(tput setaf 5)
+export bold=$(tput bold)
+export reset=$(tput sgr0)
+
+# Create the config directory if it doesn't exist
 if [[ ! -d "${config_dir}" ]]; then
-    echo "Creating ${config_dir}"
+    echo "${green}Creating ${bold}${config_dir}${reset}"
     mkdir -p "${config_dir}"
 fi
 
 # Let's use a bash function instead.
 function convert_and_save() {
     local now=$(date +"%Y-%m-%d %H:%M:%S")
-
-    local config_json="${config_dir}/config.json"
-    local powerline_yaml="${script_dir}/powerline.yaml"
-
-    local red=$(tput setaf 1)
-    local green=$(tput setaf 2)
-    local bold=$(tput bold)
-    local reset=$(tput sgr0)
-
-    echo "${bold}${now}: Detected changes in ${powerline_yaml}${reset}"
-
+    echo "[${now}] Detected changes in ${bold}${powerline_yaml}${reset}"
     yq -o json "${powerline_yaml}" | jq -r .config > "${config_json}"
-
-    echo "${green}Saved new ${config_json}${reset}"
+    echo "${green}Saved new ${bold}${config_json}${reset}"
 }
 
 # Export the function so it's available to subshells
 export -f convert_and_save
 
 # Monitor for changes in the YAML file and convert it to JSON
-fswatch -o powerline.yaml | xargs -n1 -I{} bash -c 'convert_and_save'
+echo "${magenta}Watching for changes in ${bold}${powerline_yaml}${reset}"
+fswatch -o "${powerline_yaml}" | xargs -n1 -I{} bash -c 'convert_and_save'
