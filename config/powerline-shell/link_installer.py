82cb88d5d94c09e5c5074a71dcaeb3493b108fb0
Luis Armendariz <luis.armendariz@gmail.com>
2024-04-13 18:50:20 -0700

    Add link_installer.py for powerline-shell configs

---
+++ config/powerline-shell/link_installer.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+from pathlib import Path
+
+import typer
+from rich.console import Console
+
+app = typer.Typer()
+console = Console()
+
+
+def unexpanduser(path: Path) -> Path:
+    return Path(str(path).replace(str(Path.home()), "~"))
+
+
+def link_path(source: Path, target: Path):
+    fmt = {
+        "source": unexpanduser(source),
+        "target": unexpanduser(target),
+    }
+    if not target.exists():
+        target.symlink_to(source)
+        msg = (
+            "Created new symlink "
+            "[green bold]{target}[/green bold] -> "
+            "[green bold]{source}[/green bold]"
+        )
+        console.print(msg.format(**fmt))
+        return
+    if target.is_dir():
+        msg = "Directory already exists: [magenta bold]{target}"
+        console.print(msg.format(**fmt))
+    elif target.is_symlink():
+        old_source = target.readlink().resolve()
+        if old_source == source.resolve():
+            msg = (
+                "Symlink is up to date: "
+                "[magenta bold]{target}[/magenta bold] -> "
+                "[magenta bold]{source}[/magenta bold]"
+            )
+            console.print(msg.format(**fmt))
+            return
+        target.unlink()
+        target.symlink_to(source)
+        fmt["old_source"] = unexpanduser(old_source)
+        msg = (
+            "Updated symlink with "
+            "[magenta bold]{target}[/magenta bold] -> "
+            "[magenta bold]{source}[/magenta bold]"
+            "Removed older source "
+            "[magenta bold]{old_source}[/magenta bold]"
+        )
+        console.print(msg.format(**fmt))
+
+
+@app.command()
+def install_links():
+    script_dir = Path(__file__).resolve().parent
+    target_dir = Path("~/.config/powerline-shell").expanduser()
+
+    if not target_dir.exists():
+        target_dir.mkdir(parents=True, exist_ok=True)
+        console.print(f"[green]Created new directory [bold]{target_dir}[/bold]")
+
+    manifest = ["config.json", "themes", "segments"]
+    for item in manifest:
+        source = script_dir / item
+        target = target_dir / item
+        link_path(source, target)
+
+
+if __name__ == "__main__":
+    app()
