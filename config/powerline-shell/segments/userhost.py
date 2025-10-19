8f0853077a148c9003bbf6acef3d2613bf0635c8
Luis Armendariz <luis.armendariz@gmail.com>
2025-10-19 14:22:24 -0700

    Use host-specific color overrides in userhost segment
    Check host_color_lut first and use its (fg, bg) for matching hostnames;
    fall back to hashed color generation and rgb2short conversion otherwise.
    Fixes unreadable colors for known hosts (eg. yappa).

--- config/powerline-shell/segments/userhost.py
+++ config/powerline-shell/segments/userhost.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # type: ignore
 """Segment to show `user@host` on the prompt."""
+
 import os
 import getpass
 from socket import gethostname
@@ -10,14 +11,24 @@ from powerline_shell.color_compliment import stringToHashToColorAndOpposite
 from powerline_shell.colortrans import rgb2short
 
 
+# These are (fg_short, bg_short) tuples to use as exceptions.
+# For example, the `yappa`` userhost segment was unreadable.
+host_color_lut = {
+    "yappa": (255, 131),  # from (96, 131)
+}
+
+
 class Segment(BasicSegment):
     def add_to_powerline(self):
         powerline = self.powerline
 
         # Let's always colorize our segment.
         hostname = gethostname()
-        fg, bg = stringToHashToColorAndOpposite(hostname)
-        fg, bg = (rgb2short(*color) for color in (fg, bg))
+        if hostname in host_color_lut:
+            fg, bg = host_color_lut[hostname]
+        else:
+            fg, bg = stringToHashToColorAndOpposite(hostname)
+            fg, bg = (rgb2short(*color) for color in (fg, bg))
 
         if powerline.args.shell == "bash":
             userhost_prompt = r" \u@\h "
